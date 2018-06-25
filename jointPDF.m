3fe08c8ff22510c7220eb4ca8f7e966171679af6
Benoit Pasquier <pasquieb@uci.edu>
2018-06-25 13:55:12 -0700

    added a handle to the output of jointPDF

--- jointPDF.m
+++ jointPDF.m
@@ -1,8 +1,8 @@
-function jointPDF(xobs,xmod,v) 
+function h = jointPDF(xobs,xmod,v) 
 % jointPDF(xobs,xmod,v)
 
 [D, X, Y] = mk2dcpdf(xobs, xmod, 1:numel(xmod), v) ;
 clevs = 0:.1:1 ;
-contourf(X(1,:),Y(:,1),D,clevs,'LineStyle','none') ;
+h = contourf(X(1,:),Y(:,1),D,clevs,'LineStyle','none') ;
 %caxis(minmax2(clevs))
 
